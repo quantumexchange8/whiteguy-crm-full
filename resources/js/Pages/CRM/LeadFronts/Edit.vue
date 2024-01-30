a689b3b1d10280ab1ce50af9112d5daf6a036e77
Daniel Tan <72296991+danieltky@users.noreply.github.com>
2024-01-30 17:57:13 +0800

    added edit page for lead front module

---
+++ resources/js/Pages/CRM/LeadFronts/Edit.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import Breadcrumbs from '@/Components/Breadcrumbs.vue'
+import EditLeadFrontForm from './Partials/EditLeadFrontForm.vue'
+
+const pageTitle = "Edit Lead Front";
+</script>
+
+<template>
+	<AuthenticatedLayout 
+		title="Edit Lead Front"
+	>
+		<template #header>
+			<div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+				<h2 class="text-xl font-semibold leading-tight">
+					{{ pageTitle }}
+				</h2>
+			</div>
+			<Breadcrumbs 
+				:topParentTitle="'CRM'" 
+				:parentHref="route('lead-fronts.index')" 
+				:parentTitle="'Lead Front'" 
+				:title="pageTitle"
+			/>
+		</template>
+
+		<div class="rounded-xl">
+			<EditLeadFrontForm class="w-full" />
+		</div>
+	</AuthenticatedLayout>
+</template>
